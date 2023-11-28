35261f1b4b3a1e394ce930e48a60a2a1443b8bf4
yun00045 <hayun01@dsm.hs.kr>
2023-11-28 19:44:28 +0900

    :sparkles: :: 이벤트 처리

--- reactlecture/src/component/Hello.js
+++ reactlecture/src/component/Hello.js
@@ -5,11 +5,35 @@
 import World from "./World";
 
 export default function Hello() {
+  function showName() {
+    console.log("hayun");
+  }
+  function showAge(age) {
+    console.log(age);
+  }
+  function showText(txt) {
+    console.log(txt); //target은 input, value는 input의 value
+  }
+
   return (
     <>
       <h3>Hello</h3>
-      <World />
-      <World />
+      {/* <World /> */}
+      <button onClick={showName}>Show Name</button>
+      <button
+        onClick={() => {
+          showAge(17);
+        }}
+      >
+        Show age
+      </button>
+      <input
+        type="text"
+        onChange={(text) => {
+          const txt = text.target.value;
+          showText(txt);
+        }}
+      />
     </>
   );
 }
